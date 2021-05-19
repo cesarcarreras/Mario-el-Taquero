06d99cc7df7c16811102f0dbdd85a43eb3f2b8bd
Cesar Carreras <cesar.carreras95@gmail.com>
2021-05-18 20:23:13 -0500

    update

--- javascript/level-1.js
+++ javascript/level-1.js
@@ -15,25 +15,25 @@ ctx.font ="italic small-caps bold 50px arial";
 
 //AUDIOS
 const audioStart = new Audio();
-audioStart.src = "/audios/start.wav";
+audioStart.src = "../audios/start.wav";
 
 const audioGameOver = new Audio();
-audioGameOver.src = "/audios/game-over2.wav";
+audioGameOver.src = "../audios/game-over2.wav";
 
 const audioShoot = new Audio();
-audioShoot.src = "/audios/shoot.wav";
+audioShoot.src = "../audios/shoot.wav";
 
 const audioCollision =  new Audio();
-audioCollision.src = "/audios/collision.wav";
+audioCollision.src = "../audios/collision.wav";
 
 const audioWinner = new Audio();
-audioWinner.src = "/audios/winner.wav";
+audioWinner.src = "../audios/winner.wav";
 
 const audioLooser = new Audio();
-audioLooser.src = "/audios/sad-trombone.wav";
+audioLooser.src = "../audios/sad-trombone.wav";
 
 const audioLevel = new Audio();
-audioLevel.src = "/audios/level-waiting.ogg";
+audioLevel.src = "../audios/level-waiting.ogg";
 
 
 // BACKGROUND
@@ -44,12 +44,12 @@ class Background{
         this.width = canvas.width;
         this.height = canvas.height;
         this.image = new Image();
-        this.image.src = "/images/otso-background.jpeg";
+        this.image.src = "../images/otso-background.jpeg";
     }
 
     gameOver(){
         let img = new Image() 
-        img.src = "/images/game-over-final.png"; 
+        img.src = "../images/game-over-final.png"; 
         setTimeout(() => { 
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         }, 700);
@@ -61,7 +61,7 @@ class Background{
 
     levelUp(){
         let image = new Image() 
-        image.src = "/images/winner-1.png"; 
+        image.src = "../images/winner-1.png"; 
         setTimeout(() => { 
          ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
         }, 700);
@@ -118,8 +118,8 @@ class Mario{
 }
 
 const marioImgs = [
-    "/images/mario1.png",
-    "/images/mario2.png"
+    "../images/mario1.png",
+    "../images/mario2.png"
 ];
 const mario = new Mario(100, 470, 120, 120, marioImgs);
 
@@ -131,7 +131,7 @@ class Tacos{
         this.width = 50;
         this.height = 35;
         this.image = new Image()
-        this.image.src = "/images/taco.png"
+        this.image.src = "../images/taco.png"
     }
 
     draw(){
@@ -158,7 +158,7 @@ class Ensaladas{
         this.height = 70;
         //imagen
         this.image = new Image();
-        this.image.src = "/images/angry-salad.png";
+        this.image.src = "../images/angry-salad.png";
     }
 
     draw(){
@@ -177,7 +177,7 @@ function animate(){
     drawSalads();
     mario.draw();
     ctx.fillText(`Enemigos: ${points}`, 800, 60);
-    ctx.fillText(`Nivel: 2`, 800, 120);
+    ctx.fillText(`Nivel: 1`, 800, 120);
     levelUp();
     if(requestID){
         requestAnimationFrame(animate);
@@ -249,7 +249,7 @@ function levelUp(){
 
 function gameStarts(){
     let image = new Image() 
-    image.src = "/images/nivel1.png"; 
+    image.src = "../images/nivel1.png"; 
     image.addEventListener('load', function(){
         ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
         audioLevel.play()
@@ -291,7 +291,7 @@ addEventListener("keydown", (event)=>{
     //Next level
     if(event.keyCode === 13){
         setTimeout(() => { 
-            window.location.replace("/levels/game-2.html");  
+            window.location.replace("../levels/game-2.html");  
         }, 1000);
         audioStart.play()
     }
